9b23bdfaaefac5dde6ab26a20fd0fd4ea117dd91
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2023-04-12 02:03:19 +0700

    fix: error missing function in main

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,4 @@ import { OpenAI } from './openai';
 const openai = new OpenAI(config.chatGpt);
 const discord = new Discord(config.discord, openai);
 
-await discord.registerCommands();
-
 await discord.start();
