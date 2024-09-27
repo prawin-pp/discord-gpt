808cc251e3ee80cec42efc3f6974a3edd6a78a40
Prawin Rungpiriyadej <prawin.ru@nocnoc.com>
2024-09-28 01:59:55 +0700

    remove default value config

--- src/config.ts
+++ src/config.ts
@@ -11,12 +11,8 @@ export const config = {
     maxTokens: parseInt(import.meta.env.VITE_CHAT_GPT_MAX_TOKENS)
   },
   apple: {
-    crontab: import.meta.env.VITE_APPLE_CRONTAB || `*/10 * * * *`,
-    link:
-      import.meta.env.VITE_APPLE_LINK ||
-      `https://www.apple.com/th/shop/fulfillment-messages?pl=true&mts.0=regular&mts.1=compact&cppart=UNLOCKED/WW&parts.0=MYWW3ZP/A&location=10150`,
-    webhook:
-      import.meta.env.VITE_APPLE_WEBHOOK ||
-      `https://discord.com/api/webhooks/1287726384273293332/RA3eYYamrXtGgY9154Q4igHdTwLqiIzBlgcWD6OqtLshwVsuKJjs_bVx_XNabMD4kVL5`
+    crontab: import.meta.env.VITE_APPLE_CRONTAB,
+    link: import.meta.env.VITE_APPLE_LINK,
+    webhook: import.meta.env.VITE_APPLE_WEBHOOK
   }
 };
